8c4aa3bb4b6f4bf5c049eb705258282b31766299
hasharanethmi2020@gmail.com <hasharanethmi2020@gmail.com>
2024-04-11 21:04:04 +0530

    Change Categories Component

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 const Categories = () => {
   return (
     <div className="categories">
-      <h1>Our Top Property List</h1>
-      <p>
+      <h1 style={{letterSpacing: "3px"}}>Our Top Property List</h1>
+      <p style={{letterSpacing: "2px" , lineHeight: "35px" , fontSize: "16px"}}>
       Welcome to your one-stop destination for home bookings. Explore luxurious villas, cozy cottages, and everything in between. Plan your next escape with ease – our user-friendly platform makes finding and booking your dream home a breeze.
       </p>
 
@@ -17,8 +17,8 @@ const Categories = () => {
               <img src={category.img} alt={category.label} />
               <div className="overlay"></div>
               <div className="category_text">
-                <div className="category_text_icon">{category.icon}</div>
-                <p>{category.label}</p>
+                <div className="category_text_icon" style={{color: "orange"}}>{category.icon}</div>
+                <p style={{letterSpacing: "2px" , color: "orange"}}>{category.label}</p>
               </div>
             </div>
           </Link>
